054ea6e7950f53908d28ffcf6ee1f0b74186ca63
PopsFPSB <popsfpsb@gmail.com>
2022-08-21 15:12:14 +0200

    [add] added endpoint to get all spinners

---
+++ frontend/src/routes/api/spinners/+server.ts
@@ -0,0 +1,12 @@
+import { PrismaClient } from "@prisma/client";
+
+/** @type {import('./$types').RequestHandler} */
+
+const prisma = new PrismaClient();
+
+export async function GET() {
+  const spinners = await prisma.spinners.findMany({});
+  return new Response(JSON.stringify(spinners), {
+    headers: new Headers({ "Content-Type": "application/json" }),
+  });
+}
