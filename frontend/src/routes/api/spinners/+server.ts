6a83634d55623327139942ea55c4a8f001faf15f
PopsFPSB <popsfpsb@gmail.com>
2022-08-21 15:23:56 +0200

    [fix] closed db connexion before returning

--- frontend/src/routes/api/spinners/+server.ts
+++ frontend/src/routes/api/spinners/+server.ts
@@ -6,6 +6,7 @@ const prisma = new PrismaClient();
 
 export async function GET() {
   const spinners = await prisma.spinners.findMany({});
+  await prisma.$disconnect();
   return new Response(JSON.stringify(spinners), {
     headers: new Headers({ "Content-Type": "application/json" }),
   });
