436f97a30efc13536a2a52da6688724c2d554af6
PopsFPSB <popsfpsb@gmail.com>
2022-08-21 16:16:52 +0200

    [add] added endpoint to add a spinner

--- frontend/src/routes/api/spinners/[key]/+server.ts
+++ frontend/src/routes/api/spinners/[key]/+server.ts
@@ -26,3 +26,38 @@ export async function GET({ params }: { params: { key: string } }) {
     headers: new Headers({ "Content-Type": "application/json" }),
   });
 }
+
+export async function POST({
+  params,
+  request,
+}: {
+  params: { key: string };
+  request: Request;
+}) {
+  const key = params.key;
+  const body = await request.formData();
+  const name = body.get("name")?.toString() ?? "";
+  const twitter = body.get("twitter")?.toString() ?? "";
+  const youtube = body.get("youtube")?.toString() ?? "";
+  const board = body.get("board")?.toString() ?? "";
+  if (name === "" || board == "") {
+    throw error(400, "Missing parameters");
+  }
+  const spinner = await prisma.spinners
+    .create({
+      data: {
+        key: key,
+        name: name,
+        twitter: twitter,
+        youtube: youtube,
+        board: board,
+      },
+    })
+    .catch((e) => {
+      throw error(500, e.message);
+    });
+  await prisma.$disconnect();
+  return new Response(JSON.stringify(spinner), {
+    headers: new Headers({ "Content-Type": "application/json" }),
+  });
+}
