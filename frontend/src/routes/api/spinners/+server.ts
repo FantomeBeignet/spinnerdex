4b72af112557e342d533a45e6c504d9b9eeb36ce
PopsFPSB <popsfpsb@gmail.com>
2022-08-21 21:36:09 +0200

    [refactor] moved board query to better endpoint

--- frontend/src/routes/api/spinners/+server.ts
+++ frontend/src/routes/api/spinners/+server.ts
@@ -1,16 +1,24 @@
+import { error } from "@sveltejs/kit";
 import { PrismaClient } from "@prisma/client";
 
 /** @type {import('./$types').RequestHandler} */
 
 const prisma = new PrismaClient();
 
-export async function GET() {
+export async function GET({ url }: { url: URL }) {
+  const board = url.searchParams.get("board")?.toString() ?? undefined;
   const spinners = await prisma.spinners.findMany({
+    where: {
+      board: board,
+    },
     orderBy: {
       key: "asc",
     },
   });
   await prisma.$disconnect();
+  if (spinners.length === 0) {
+    throw error(404, "No spinner found");
+  }
   return new Response(JSON.stringify(spinners), {
     headers: new Headers({ "Content-Type": "application/json" }),
   });
