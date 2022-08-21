5af66ee8e4246709a640e0aaf590ef83ea3f028a
BENE Tom <tom.bene@telecomnancy.eu>
2022-08-21 13:30:20 +0200

    [add] added base layout

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+  import "../app.css";
+</script>
+
+<div class="min-h-screen bg-background-dark">
+  <nav class="bg-background-dark border-b p-3 border-background-light">
+    <span class="text-white font-bold text-2xl"> SpinnerDex </span>
+  </nav>
+
+  <slot />
+
+  <footer
+    class="fixed bottom-0 w-full flex items-center justify-center text-background-light p-1"
+  >
+    By Pops (FPSB) - 2022
+  </footer>
+</div>
