f629cd98e6c9d8b38eea7b27bba8794ec658d6d9
PopsFPSB <popsfpsb@gmail.com>
2022-08-22 22:12:40 +0200

    [style] removed footer and improved navbar sizing

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -3,15 +3,9 @@
 </script>
 
 <div class="min-h-screen bg-background-dark">
-  <nav class="bg-background-dark border-b p-3 border-background-light">
-    <img src="/text.png" alt="SpinnerDex logotype" class="h-8 md:h-16" />
+  <nav class="bg-background-dark border-b p-3 md:p-4 border-background-light">
+    <img src="/text.png" alt="SpinnerDex logotype" class="h-7 md:h-12" />
   </nav>
 
   <slot />
-
-  <footer
-    class="fixed bottom-0 w-full flex items-center justify-center text-background-light p-1"
-  >
-    By Pops (FPSB) - 2022
-  </footer>
 </div>
