2aca9f6e863c56b7cefbcdbde3b7f356c9d01293
PopsFPSB <popsfpsb@gmail.com>
2022-08-23 01:23:08 +0200

    [style] improved responsiveness

--- frontend/src/routes/spinners/[key]/+page.svelte
+++ frontend/src/routes/spinners/[key]/+page.svelte
@@ -16,24 +16,26 @@
   const profilePicture = data.profilePicture;
 </script>
 
-<main class="bg-background-dark grid place-items-center mt-20 mx-10">
+<main class="bg-background-dark grid place-items-center my-20 mx-10 md:m-32">
   <div
-    class="text-white border-background-light rounded-md border-2 flex flex-col items-center justify-center w-9/12 px-8 pb-8"
+    class="text-white border-background-light rounded-md border-2 flex flex-col items-center justify-center w-9/12 md:w-fit px-8 md:px-24 pb-8 md:pb-12"
   >
     <img
       src={profilePicture}
       alt={spinner.name + "'s profile picture"}
-      class="-translate-y-1/2 rounded-full w-20 h-20"
+      class="-translate-y-1/2 rounded-full w-20 md:w-28 h-20 md:h-28"
     />
-    <div class="flex flex-col gap-6 overflow-hidden">
-      <h1 class="font-semibold text-xl text-center">{spinner.name}</h1>
+    <div class="flex flex-col gap-6 md:gap-10 overflow-hidden">
+      <h1 class="font-semibold text-xl md:text-2xl text-center">
+        {spinner.name}
+      </h1>
       <div
-        class="flex flex-col items-start justify-center gap-3 text-center w-full overflow-hidden"
+        class="flex flex-col items-start justify-center gap-3 md:gap-4 text-center w-full"
       >
-        <span class="flex gap-2 items-center justify-start">
+        <span class="flex gap-2 md:gap-3 items-center justify-start">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            class="h-6 w-6 text-white"
+            class="h-6 md:h-8 w-6 md:w-8 text-white"
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
@@ -45,26 +47,30 @@
               d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"
             />
           </svg>
-          <p class="justify-self-center">{spinner.board}</p></span
+          <p class="md:text-lg">{spinner.board}</p></span
         >
-        <span class="flex gap-2 items-center justify-start">
-          <img src="/twitter.svg" alt="twitter logo" class="h-6 w-6 inline" />
+        <span class="flex gap-2 md:gap-3 items-center justify-start">
+          <img
+            src="/twitter.svg"
+            alt="twitter logo"
+            class="h-6 md:h-8 w-6 md:w-8 inline"
+          />
           {#if spinner.twitter != ""}
-            <a href={spinner.twitter} class="block">{twitterAt}</a>
+            <a href={spinner.twitter} class="block md:text-lg">{twitterAt}</a>
           {:else}
-            <p class="italic">{twitterAt}</p>
+            <p class="italic md:text-lg">{twitterAt}</p>
           {/if}
         </span>
-        <span class="flex gap-2 items-center justify-start">
-          <img src="/youtube.svg" alt="youtube logo" class="h-6 w-6" />
+        <span class="flex gap-2 md:gap-3 items-center justify-start">
+          <img
+            src="/youtube.svg"
+            alt="youtube logo"
+            class="h-6 md:h-8 w-6 md:w-8"
+          />
           {#if spinner.youtube != ""}
-            <a
-              href={spinner.youtube}
-              class="block text-ellipsis whitespace-nowrap overflow-hidden"
-              >YouTube link</a
-            >
+            <a href={spinner.youtube} class="block md:text-lg">YouTube link</a>
           {:else}
-            <p class="italic">No YouTube found</p>
+            <p class="italic md:text-lg">No YouTube found</p>
           {/if}
         </span>
       </div>
