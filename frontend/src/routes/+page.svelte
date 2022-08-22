b193ff624178278b00bbc0c54e0ce88cf5086cda
PopsFPSB <popsfpsb@gmail.com>
2022-08-22 22:32:26 +0200

    [add] added button to search page on home page

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -14,5 +14,10 @@
       This website is aimed towards referencing as many spinners as possible,
       along with their board and their Twitter/YouTube links.
     </p>
+    <a
+      href="/spinners"
+      class="bg-background-dark border-2 border-primary rounded-sm text-white p-3 md:p-4 md:text-xl md:rounded hover:underline"
+      >Search for spinners</a
+    >
   </div>
 </main>
