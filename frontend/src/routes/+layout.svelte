74a7300e322b15da327947655eee7c5bccecb10d
PopsFPSB <popsfpsb@gmail.com>
2022-08-22 22:14:11 +0200

    [add] made navbar logo a link to the homepage

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -4,7 +4,9 @@
 
 <div class="min-h-screen bg-background-dark">
   <nav class="bg-background-dark border-b p-3 md:p-4 border-background-light">
-    <img src="/text.png" alt="SpinnerDex logotype" class="h-7 md:h-12" />
+    <a href="/"
+      ><img src="/text.png" alt="SpinnerDex logotype" class="h-7 md:h-12" /></a
+    >
   </nav>
 
   <slot />
