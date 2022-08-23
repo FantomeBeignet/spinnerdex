b4f786f403afc75e21e45397b8d64c4cf608a41c
PopsFPSB <popsfpsb@gmail.com>
2022-08-24 00:45:39 +0200

    [add] added function to authenticate user

---
+++ frontend/src/lib/auth.ts
@@ -0,0 +1,40 @@
+import bcrypt from "bcrypt";
+import { PrismaClient } from "@prisma/client";
+
+export async function auth(request: Request) {
+  const authHeader = request.headers.get("Authorization");
+  let id;
+  let key;
+  if (authHeader !== null) {
+    const [type, value] = authHeader.split(" ");
+    if (type === "Basic") {
+      [id, key] = Buffer.from(value, "base64").toString().split(":");
+    }
+  } else if (new URL(request.url).searchParams.has("api_key")) {
+    const apiKey = new URL(request.url).searchParams.get("api_key")!.toString();
+    [id, key] = Buffer.from(apiKey, "base64").toString().split(":");
+  } else {
+    return { code: 401, message: "Unauthorized" };
+  }
+  const saltRounds = 10;
+  const keyHash = await bcrypt.hash(key as string, saltRounds);
+  const prisma = new PrismaClient();
+  const contributor = await prisma.contributor.findFirst({
+    where: {
+      id: id,
+    },
+    select: {
+      keyHash: true,
+      role: true,
+    },
+  });
+  if (contributor === null) {
+    return { code: 401, message: "Unauthorized" };
+  }
+  const compare = await bcrypt.compare(keyHash, contributor.keyHash);
+  if (!compare) {
+    return { code: 403, message: "Forbidden" };
+  }
+  await prisma.$disconnect();
+  return { code: 200, message: "OK", userId: id, role: contributor.role };
+}
